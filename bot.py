96196c433420d5c870d04ea1c38ed0cc950db541
socr102 <normanburtonfree@gmail.com>
2022-02-08 20:20:14 +0900

    update bot

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import pandas as pd
 cg = CoinGeckoAPI()
 ids='bitcoin'
 def get_price():
-    coin_market = cg.get_coins_markets(vs_currency='usd'))
+    coin_market = cg.get_coins_markets(vs_currency='usd')
     df_market = pd.DataFrame(coin_market,columns=['id','current_price','market_cap','total_volume','fully_diluted_valuation'])
     df_market.set_index('id',inplace=True)
     df_market
