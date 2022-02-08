7df20b15ced19078aa77c0e5efdd07789aa2bde6
socr102 <normanburtonfree@gmail.com>
2022-02-08 21:18:53 +0900

    update

--- bot.py
+++ bot.py
@@ -17,7 +17,8 @@ def get_coin_data():
     for i in range(len(coin_market)):
         name.append(coin_market[i]['id'])
         price.append(coin_market[i]['current_price'])
-        roc.append(str(1/float(coin_values[i]['value'])))
+        rate = float(coin_values[i]['value'])
+        roc.append(float(1/rate))
         volume.append(coin_market[i]['total_volume'])
         mcap.append(coin_market[i]['market_cap'])
         fdv.append(coin_market[i]['fully_diluted_valuation'])
