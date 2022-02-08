6ac9f994170b8e5f533213d7e8659be23f47b16a
socr102 <normanburtonfree@gmail.com>
2022-02-08 20:46:28 +0900

    update

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from pycoingecko import CoinGeckoAPI
 import pandas as pd 
 cg = CoinGeckoAPI()
 def get_price():
-    coin_market = cg.get_coins_markets(vs_currency='usd'))
+    coin_market = cg.get_coins_markets(vs_currency='usd')
     df_market = pd.DataFrame(coin_market,columns=['id','current_price','market_cap','total_volume','fully_diluted_valuation'])
     coinexcel = pd.ExcelWriter('Coin.xlsx')
     # write DataFrame to excel
