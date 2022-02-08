2610025775b1fbff09a4d53915b3dc996bc92b9e
socr102 <normanburtonfree@gmail.com>
2022-02-08 20:53:37 +0900

    update

--- bot.py
+++ bot.py
@@ -5,4 +5,5 @@ def get_price():
     coin_market = cg.get_coins_markets(vs_currency='usd')
     df_market = pd.DataFrame(coin_market,columns=['id','current_price','market_cap','total_volume','fully_diluted_valuation'])
     df_market.to_csv('coin.csv')
+    print(cg.get_exchange_rates())
 get_price()
