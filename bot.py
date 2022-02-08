7a85cb424a118cc2d0f3ba2faa90ea888c13450d
socr102 <normanburtonfree@gmail.com>
2022-02-08 20:48:34 +0900

    update

--- bot.py
+++ bot.py
@@ -4,10 +4,5 @@ cg = CoinGeckoAPI()
 def get_price():
     coin_market = cg.get_coins_markets(vs_currency='usd')
     df_market = pd.DataFrame(coin_market,columns=['id','current_price','market_cap','total_volume','fully_diluted_valuation'])
-    coinexcel = pd.ExcelWriter('Coin.xlsx')
-    # write DataFrame to excel
-    cars_data.to_excel(coinexcel)
-    # save the excel
-    coinexcel.save()
-    print('DataFrame is written to Excel File successfully.')
+    df_market.to_csv('coin.csv')
 get_price()
